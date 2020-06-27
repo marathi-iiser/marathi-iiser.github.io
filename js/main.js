01a46c1eeeb64e99624bec3580dde857b9ae6d9e
Maxcool-In <Maxcool-In@users.noreply.github.com>
2020-06-27 14:19:21 +0530

    final upadtes

--- js/main.js
+++ js/main.js
@@ -103,19 +103,19 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  // Porfolio filter
-  $("#portfolio-flters li").click(function () {
-    $("#portfolio-flters li").removeClass('filter-active');
+  // gallery filter
+  $("#gallery-flters li").click(function () {
+    $("#gallery-flters li").removeClass('filter-active');
     $(this).addClass('filter-active');
 
     var selectedFilter = $(this).data("filter");
-    $("#portfolio-wrapper").fadeTo(100, 0);
+    $("#gallery-wrapper").fadeTo(100, 0);
 
-    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+    $(".gallery-item").fadeOut().css('transform', 'scale(0)');
 
     setTimeout(function () {
       $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
-      $("#portfolio-wrapper").fadeTo(300, 1);
+      $("#gallery-wrapper").fadeTo(300, 1);
     }, 300);
   });
 
